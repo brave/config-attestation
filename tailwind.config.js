30b5e0a4a52d0c398b55e1697bf639c24d1ab9b3
Jacob Lamont <cob@jacoblamont.com>
2024-09-04 02:20:01 -0400

    chore(styles): update nala (#29)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ["./**/*.html"],
-	presets: [
-		require("@brave/leo/build/tailwind")
-	],
-	theme: {},
+	presets: [require("@brave/leo/tokens/tailwind")],
+	theme: {
+		extend: {
+			screens: {
+				"2xl": "1440px",
+			},
+		},
+	},
 	plugins: [],
 };
